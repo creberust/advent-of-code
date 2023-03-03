7733a694fc51d544ebd6d34fe72916e0ad723dd3
Creberus <creberus@tutanota.com>
2023-03-04 00:00:39 +0100

    feat(common): add year, day, part and event, puzzle abstraction

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// The Advent of Code year
+pub struct Year(u16);
+
+/// The day of the puzzle
+pub struct Day(u8);
+
+/// The part of the puzzle to solve
+pub enum Part {
+    /// Only the first part
+    One,
+    /// Only the second part
+    Two,
+    /// Both of the parts
+    Both,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Abstraction for an AoC event (year)
+pub trait Event {
+    /// Solve the given day for an AoC event
+    ///
+    /// # Parameters
+    /// * `day` - The day of the event to solve
+    /// * `part` - The part(s) of the puzzle to solve
+    fn solve(day: Day, part: Part);
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Abstraction for an AoC puzzle
+pub trait Puzzle {
+    /// Solve the `part` of the puzzle
+    ///
+    /// # Parameters
+    /// * `part` - The part(s) of the puzzle to solve
+    fn solve(part: Part);
 }
