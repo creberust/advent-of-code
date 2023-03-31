fdb4f63f590b3473fde8f86f6c00a5786eef0143
Creberus <creberus@tutanota.com>
2023-03-31 21:54:52 +0100

    feat(puzzle): add Eq and Hash trait for puzzle

--- common/src/puzzle.rs
+++ common/src/puzzle.rs
@@ -1,8 +1,9 @@
-use std::fmt::Display;
+use std::{fmt::Display, hash::Hash};
 
 use crate::*;
 
 /// The puzzle of the given day for a given event.
+#[derive(Debug, Eq)]
 pub struct Puzzle<S: Fn(&Input)> {
     day: Day,
     name: String,
@@ -34,6 +35,7 @@ impl<S: Fn(&Input)> Puzzle<S> {
     /// Solve the `part` of the puzzle
     ///
     /// # Parameters
+    /// * `input` - The input given to the puzzle
     /// * `part` - The part(s) of the puzzle to solve
     pub fn solve(&self, input: &Input, part: Part) {
         match part {
@@ -56,5 +58,17 @@ impl<S: Fn(&Input)> Puzzle<S> {
 impl<S: Fn(&Input)> Display for Puzzle<S> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Day {}: {}", self.day(), self.name())
+    }
+}
+
+impl<S: Fn(&Input)> PartialEq for Puzzle<S> {
+    fn eq(&self, other: &Self) -> bool {
+        self.day == other.day
+    }
+}
+
+impl<S: Fn(&Input)> Hash for Puzzle<S> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.day.hash(state)
     }
 }
