97fa0a56c6ca8179fcf87b196eab41aff44ee9dc
Benjamin Peter <creberus@tutanota.com>
2023-09-02 16:12:08 +0200

    feat(2015: 1): add implementation for second part

--- aoc-2015/src/day_1/part_2.rs
+++ aoc-2015/src/day_1/part_2.rs
@@ -6,7 +6,28 @@ pub struct Solver;
 
 impl Solution for Solver {
     fn solve(&self, input: &Input) -> i64 {
-        0
+        let mut result = 0;
+        let mut position = 1;
+
+        for line in input.read().lines() {
+            let line = line.unwrap();
+
+            for c in line.chars() {
+                match c {
+                    '(' => result += 1,
+                    ')' => result -= 1,
+                    _ => continue,
+                }
+
+                if result < 0 {
+                    break;
+                }
+
+                position += 1;
+            }
+        }
+
+        position
     }
 }
 
@@ -14,6 +35,24 @@ impl Solution for Solver {
 mod tests {
     use super::*;
 
+    fn solve(input: impl AsRef<str>, expected: i64) {
+        // Given
+        let input = Input::Text(String::from(input.as_ref()));
+
+        // When
+        let result = Solver.solve(&input);
+
+        // Then
+        assert_eq!(result, expected);
+    }
+
     #[test]
-    fn simple() {}
+    fn one() {
+        solve(")", 1);
+    }
+
+    #[test]
+    fn five() {
+        solve("()())", 5);
+    }
 }
