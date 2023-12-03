a8f16b7a5af392ac48a55481bde808fe84a31562
Benjamin Peter <benji@2a01cb080ab9b500ba65f74be9268e55.ipv6.abo.wanadoo.fr>
2023-12-03 22:41:26 +0100

    feat(2023: 1): add implementation for first part

---
+++ aoc-2023/src/day_1/part_2.rs
@@ -0,0 +1,16 @@
+use std::collections::BinaryHeap;
+use std::io::{BufRead, Lines};
+
+use common::Input;
+
+pub fn solve(input: &Input) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {}
+}
